2d2517839a257a84c7b90d63458850d8bb52567d
Diabekki <bekkitubridy@gmail.com>
2021-07-21 18:42:09 +0000

    Decision ABBA, ABBB & ABBX completed and tested. Additional commenting added. Ending 6,7,8 & 9 complete.

--- run.py
+++ run.py
@@ -6,14 +6,14 @@ print("Welcome", user, ", this is a horror-adventure game based on a Japenese\
 
 # This is the game start to test the users understanding of the 'a'/'b' option.
 # Introduction
-answer = input("Are you ready to begin? (a)begin (b)quit\n").lower()
+answer = input("Are you ready to begin? \n(a)begin (b)quit\n").lower()
 
 if answer == "a":  # Decision A
     answer = input("You arrive at your local train station after finishing \
 a late shift, it was not a common occurance for you to work overtime so \
 you have not walked the streets back home alone at night before. \
 After exiting the train station you decide to walk your usual day time \
-route home but notice road works have closed the route so you decide to: \
+route home but notice road works have closed the route so you decide to:\n\
 (a)Jump the railing and continue on your path. \
 (b)Find an alternative route home.\n")
 
@@ -34,22 +34,22 @@ Thank you for playing. I hope you enjoyed. You got ending 9/9\n")
             print("INSERT BRANCH B HERE")
 
         else:  # Decision AAX
-            print("Surely you have the hang of it by now?. You lose. \
+            print("Surely you have the hang of it by now?. You lose. \n \
                 HINT: Just type a or b")
 
     elif answer == "b":  # Decision AB
         answer = input("You find a nearby alleyway to try \
 and get around the roadworks. You continue through the alleyway and meet a \
-deadend where you have to choose to go: (a) left (b) right\n")
+deadend where you have to choose to go: \n(a) left (b) right\n")
         if answer == "a":  # Decision ABA
             answer = input("You decided to go left and find yourself wondering through \
-what seems to be an endless labyrinth you decide to \
+what seems to be an endless labyrinth you decide to \n\
 (a) keep going forward (b) turn back.\n")
             if answer == "a":  # Decision ABAA (Ending 7/9)
                 print("As you proceed, you hear a noise behind you\
 , you begin to run this noise becomes louder and louder. \
 You trip, a set of glowing red eyes drag themselves up your legs \
-as you feel a set of cold hands on you. Darkness fades over you. \
+as you feel a set of cold hands on you. Darkness fades over you.\n\
 Thank you for playing, I hope you enjoyed. You got ending 7/9\n")
             elif answer == "b":  # Decision ABAB (Ending 8/9)
                 print("As you turn around to go back you notice \
@@ -61,9 +61,27 @@ Thank you for playing, I hope you enjoyed. You got ending 8/9\n")
             else:  # Decision ABAX
                 print("I think you're messing now. You lose!.")
         elif answer == "b":  # Decision ABB
-            print("After continuing for a few minutes you exit the alleyway in the \
-local market street. You hear a strange scratching noices coming from down \
-the street. You (a) go investigate the noise. (b) keep heading home.\n")
+            answer = input("After continuing for a few minutes you exit the \
+alleyway in the local market street. You hear a strange scratching noises \
+coming from down the street. You \n\
+(a) go investigate the noise. (b) keep heading home.\n")
+            if answer == "a":  # Decision ABBA (Ending 6/9)
+                print("You decided to walk down towards the strange noise \
+to investiogate. You turn towards a dark alleyway where the noise seems \
+to come from. As your surroundings come into vision you notice what \
+seems to be a young girl in a school uniform missing her lower torso. \
+The young girls head turns to see you. She begins dragging herself \
+along the ground towards you the scratching getting louder \
+and faster.\n \
+Thank you for playing and I hope you enjoyed. You got ending 6/9")
+            elif answer == "b":  # Decision ABBB
+                answer = input("You continue on your way home with \
+a feeling of being watched. You quicken your pace, the scratching \
+noise you hear seems to be getting louder dispite heading in the \
+opposite direction.You notice a low wall you could possibly hide \
+behind, you decide to \n(a) run. (b) hide.\n")
+            else:  # Decision ABBX
+                print("I think you're messing now. You lose!.")
 
         else:  # Decision ABX
             print("C'mon now, you can do better than that cant you?. You lose. \
