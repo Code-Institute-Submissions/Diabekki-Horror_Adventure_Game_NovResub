dd730ca6ad7893867984029a6d25f428490398e5
Diabekki <bekkitubridy@gmail.com>
2021-11-09 15:47:08 +0000

    Wire up APIs and give access to google sheets

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('game_survey')
+
+survey = SHEET.worksheet('survey')
+
+survey_data = survey.get_all_values()
+
+print(survey_data)
+
 # Collecting the users name
 user = input("What is your name? \n")
 if user == "":
